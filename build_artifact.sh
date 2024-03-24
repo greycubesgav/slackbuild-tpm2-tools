47147096fde2ac0b2b78c10defe4a1937e33b00e
Gavin Brown <gav@greycubes.net>
2024-03-24 14:29:16 +0200

    Fix to cleanup temp_container after extract

--- build_artifact.sh
+++ build_artifact.sh
@@ -4,4 +4,5 @@ docker build -t grecycubesgav/slackbuild-tpm2-tools:latest .
 docker create --name temp_container grecycubesgav/slackbuild-tpm2-tools:latest
 mkdir -p packages
 docker cp temp_container:/tmp/tpm2-tools-5.6-x86_64-GG_GG.tgz ./packages/
+docker rm temp_container
 md5sum ./packages/*.*
